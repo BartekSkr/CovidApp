5dc87b11ba77f2e9bda994f44b79ef507590d5c1
Bartek <bartoszskorka86@gmail.com>
2022-02-06 14:55:47 +0100

    code cleanup

--- src/app/components/cases/Country.tsx
+++ src/app/components/cases/Country.tsx
@@ -1,78 +1,99 @@
-import './Cases.scss'
-import { faFlag } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { ChangeEvent, useEffect, useState } from 'react'
-import { Spinner } from '../ui/common/Spinner/Spinner'
-import { dataFormat } from '../../helpers/helpers'
+import './Cases.scss';
+import { faFlag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { Spinner } from '../ui/common/Spinner/Spinner';
+import { dataFormat } from '../../helpers/helpers';
 
-interface CountryDataInterface{
+interface CountryDataInterface {
   countryData: {
-    country: string,
-    todayCases: number,
-    todayDeaths: number,
-    cases: number,
-    deaths: number,
-    recovered: number
-  }
+    country: string;
+    todayCases: number;
+    todayDeaths: number;
+    cases: number;
+    deaths: number;
+    recovered: number;
+  };
 }
 
+const countries: { name: string }[] = [
+  { name: 'Poland' },
+  { name: 'Austria' },
+  { name: 'Belgium' },
+  { name: 'Canada' },
+  { name: 'Croatia' },
+  { name: 'Czechia' },
+  { name: 'Denmark' },
+  { name: 'Germany' },
+  { name: 'Israel' },
+  { name: 'Japan' },
+  { name: 'Spain' },
+  { name: 'Switzerland' },
+  { name: 'UK' },
+  { name: 'USA' },
+];
+
 export const Country: React.FC = () => {
-  const [countryData, setCountryData] = useState<CountryDataInterface['countryData']>(
-    { country: '', todayCases: 0, todayDeaths: 0, cases: 0, deaths: 0, recovered: 0 })
-  const [loading, setLoading] = useState<boolean>(false)
-  const [countries] = useState<{name: string}[]>([
-    { name: 'Poland' },
-    { name: 'Austria' },
-    { name: 'Belgium' },
-    { name: 'Canada' },
-    { name: 'Croatia' },
-    { name: 'Czechia' },
-    { name: 'Denmark' },
-    { name: 'Germany' },
-    { name: 'Israel' },
-    { name: 'Japan' },
-    { name: 'Spain' },
-    { name: 'Switzerland' },
-    { name: 'UK' },
-    { name: 'USA' },
-  ])
+  const [countryData, setCountryData] = useState<
+    CountryDataInterface['countryData']
+  >({
+    country: '',
+    todayCases: 0,
+    todayDeaths: 0,
+    cases: 0,
+    deaths: 0,
+    recovered: 0,
+  });
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    setLoading(true)
-    fetch(`https://coronavirus-19-api.herokuapp.com/countries/${countries[0].name}`)
-      .then(res => res.json())
+    setLoading(true);
+    fetch(
+      `https://coronavirus-19-api.herokuapp.com/countries/${countries[0].name}`
+    )
+      .then((res) => res.json())
       .then((data: CountryDataInterface['countryData']) => {
-        setCountryData(data)
-        setLoading(false)
+        setCountryData(data);
+        setLoading(false);
       })
-      .catch(err => console.error(err))
-  }, [countries])
+      .catch((err: Error) => console.error(err));
+  }, []);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>): void => {
-    fetch(`https://coronavirus-19-api.herokuapp.com/countries/${e.target.value}`)
-      .then(res => res.json())
+    fetch(
+      `https://coronavirus-19-api.herokuapp.com/countries/${e.target.value}`
+    )
+      .then((res) => res.json())
       .then((data: CountryDataInterface['countryData']) => {
-        setCountryData(data)
+        setCountryData(data);
       })
-      .catch((err: Error) => console.error(err))
-  }
+      .catch((err: Error) => console.error(err));
+  };
 
   const Select = () => {
     return (
-      <select className="select" value={countryData.country} onChange={handleChange}>
-        {countries.map(country => (
-          <option key={`key-${country.name}`} value={country.name}>{country.name}</option>
+      <select
+        className='select'
+        value={countryData.country}
+        onChange={handleChange}
+      >
+        {countries.map((country) => (
+          <option key={`key-${country.name}`} value={country.name}>
+            {country.name}
+          </option>
         ))}
       </select>
-    )
-  }
+    );
+  };
 
   if (loading) {
-    return <Spinner />
+    return <Spinner />;
   } else {
     return (
       <main className='country'>
-        <h3><FontAwesomeIcon icon={faFlag} /> <Select /></h3>
+        <h3>
+          <FontAwesomeIcon icon={faFlag} /> <Select />
+        </h3>
         <h4>Today's cases:</h4>
         <p>{dataFormat(countryData.todayCases)}</p>
         <h4>Today's deaths:</h4>
@@ -84,6 +105,6 @@ export const Country: React.FC = () => {
         <h4>Total recovered:</h4>
         <p>{dataFormat(countryData.recovered)}</p>
       </main>
-    )
+    );
   }
-}
+};
