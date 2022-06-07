c6c2d6fb68a911d0b1ee4d374037fc94581d55f1
Bartosz Skórka <bartosz.skorka@monogo.pl>
2022-06-07 12:43:23 +0200

    back button added

--- src/pages/404-page.tsx
+++ src/pages/404-page.tsx
@@ -1,15 +1,19 @@
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import './Pages.scss';
 
-export const PageNotFound:React.FC = () => {
+export const PageNotFound: React.FC = () => {
   useEffect(() => {
     document.title = 'Error 404 (Not Found)';
   }, []);
 
   return (
-    <main className='not-found-container'>
+    <main className="not-found-container">
       <h1>404</h1>
       <h3>Sorry, we couldn't find this page.</h3>
+      <Link to="/cases">
+        <button className="button">&larr; Go back</button>
+      </Link>
     </main>
   );
 };
